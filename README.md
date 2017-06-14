d565f8d7e676dd231fa4802c95bbb32f89671379
JonLuca DeCaro <jdecaro@usc.edu>
2017-06-14 06:45:32 -0700

    Updated for new Amex Layout

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 Adds a small icon in the browser to quickly add all of the Amex offers to your currently-selected card!
 
-[Link to published version on Chrome Webstore](https://chrome.google.com/webstore)
+[Link to published version on Chrome Webstore](https://chrome.google.com/webstore/detail/fnkdinpfaifdabidnjcdgnkhfaeibbnp/)
--- js/content_script.js
+++ js/content_script.js
@@ -1,14 +1,26 @@
 
-console.log("starting");
 var loadMore = document.getElementsByClassName("ah-view-more-button");
-while (loadMore.length != 0) {
-    loadMore[0].click();
-    var loadMore = document.getElementsByClassName("ah-view-more-button");
-}
-var buttons = document.getElementsByClassName("ah-btn");
-for (var i = 0; i < buttons.length; i++) {
-    if (buttons[i].getAttribute('title') == "Add to Card") {
-        (buttons[i]).click();
+//Old amex layout 
+if (loadMore.length != 0) {
+    while (loadMore.length != 0) {
+        loadMore[0].click();
+        var loadMore = document.getElementsByClassName("ah-view-more-button");
+    }
+
+    var buttons = document.getElementsByClassName("ah-btn");
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttons[i].getAttribute('title') == "Add to Card") {
+            (buttons[i]).click();
+        }
+    }
+} else {
+    if (!window.location.href.includes("/offers/eligible")) {
+        window.location = "https://global.americanexpress.com/offers/eligible";
+    }
+    var buttons = document.getElementsByClassName("btn");
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttons[i].getAttribute('title') == "Add to Card") {
+            (buttons[i]).click();
+        }
     }
 }
-console.log("ending");
