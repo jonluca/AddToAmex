5d5426905f4a502a607f6d04e21a13523b9bbb85
JonLuca DeCaro <jdecaro@usc.edu>
2021-10-06 19:44:37 -0400

    update script

--- js/content_script.js
+++ js/content_script.js
@@ -1,31 +1,15 @@
-
-var loadMore = document.getElementsByClassName("ah-view-more-button");
-//Old page layout
-if (loadMore.length != 0) {
-    //Load all offers onto page
-    while (loadMore.length != 0) {
-        loadMore[0].click();
-        var loadMore = document.getElementsByClassName("ah-view-more-button");
-    }
-    //Find all buttons on page
-    var buttons = document.getElementsByClassName("ah-btn");
-    for (var i = 0; i < buttons.length; i++) {
-        //If the button is an Add to Card offer, click it
-        if (buttons[i].getAttribute('title') == "Add to Card") {
-            (buttons[i]).click();
-        }
-    }
-} else {
-    //New layout uses single page 
-    //url might be different for different countries, verify that it always works
+var run = () => {
     if (!window.location.href.includes("/offers/eligible")) {
+        alert("Must be on offers page, redirecting now. Please press the icon again once the page has finished loading")
         window.location = "https://global.americanexpress.com/offers/eligible";
+        return;
     }
-    //Same methodology as above
-    var buttons = document.getElementsByClassName("btn");
-    for (var i = 0; i < buttons.length; i++) {
-        if (buttons[i].getAttribute('title') == "Add to Card") {
-            (buttons[i]).click();
-        }
+    const buttons = document.querySelectorAll('[title="Add to Card"]');
+    for(const button of buttons){
+        button.click();
     }
+    // note to self - all this does is PUT to https://global.americanexpress.com/api/servicing/v1/offers/<OFFER ID>
+    // I wonder if thats a better methodology
 }
+
+run();
