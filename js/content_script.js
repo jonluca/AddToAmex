2253eb1280c29b685cfea9e1fd65228a52950bc2
JonLuca DeCaro <jdecaro@usc.edu>
2021-12-15 21:19:06 +0100

    fix add to amex by including new button text

--- js/content_script.js
+++ js/content_script.js
@@ -1,15 +1,30 @@
+var cardTexts = ["Add to Card", "Activate Offer"];
+var activateClassName = "offer-cta";
+
 var run = () => {
-    if (!window.location.href.includes("/offers/eligible")) {
-        alert("Must be on offers page, redirecting now. Please press the icon again once the page has finished loading")
-        window.location = "https://global.americanexpress.com/offers/eligible";
-        return;
-    }
-    const buttons = document.querySelectorAll('[title="Add to Card"]');
-    for(const button of buttons){
+  if (!window.location.href.includes("/offers/eligible")) {
+    alert(
+      "Must be on offers page, redirecting now. Please press the icon again once the page has finished loading"
+    );
+    window.location = "https://global.americanexpress.com/offers/eligible";
+    return;
+  }
+  const buttons = document.getElementsByTagName("button");
+  for (const button of buttons) {
+    try {
+      if (
+        cardTexts.includes(button.getAttribute("title")) ||
+        cardTexts.includes(button.innerText) ||
+        button.classList.contains(activateClassName)
+      ) {
         button.click();
+      }
+    } catch (e) {
+      console.error(e);
     }
-    // note to self - all this does is PUT to https://global.americanexpress.com/api/servicing/v1/offers/<OFFER ID>
-    // I wonder if thats a better methodology
-}
+  }
+  // note to self - all this does is PUT to https://global.americanexpress.com/api/servicing/v1/offers/<OFFER ID>
+  // I wonder if thats a better methodology
+};
 
 run();
